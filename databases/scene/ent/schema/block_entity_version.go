dc18ef9aea9cfbeedcc396f301d3bfa648c65eeb
Neko Ayaka <neko@ayaka.moe>
2026-07-28 22:57:35 +0800

    refactor(database): rename with scene

---
+++ databases/scene/ent/schema/block_entity_version.go
@@ -0,0 +1,65 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+	"github.com/proj-airi/recorder-minecraft/pkg/entsqlite"
+)
+
+type BlockEntityVersion struct {
+	ent.Schema
+}
+
+func (BlockEntityVersion) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{
+			Table: "block_entity_versions",
+			Checks: map[string]string{
+				"positive_version_id": "version_id > 0",
+				"tick_order":          "start_tick < end_tick",
+			},
+		},
+		entsqlite.Annotation{RTree: &entsqlite.RTree{
+			Name: "block_entity_versions_rtree",
+			Key:  "id",
+			Dimensions: []entsqlite.Dimension{
+				{Name: "tick", Min: "start_tick", Max: "end_tick"},
+				{Name: "x", Min: "block_x", Max: "block_x", MaxOffset: 1},
+				{Name: "y", Min: "block_y", Max: "block_y", MaxOffset: 1},
+				{Name: "z", Min: "block_z", Max: "block_z", MaxOffset: 1},
+			},
+		}},
+	}
+}
+
+func (BlockEntityVersion) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("blob", Blob.Type).Field("blob_sha256").Unique().Required(),
+	}
+}
+
+func (BlockEntityVersion) Fields() []ent.Field {
+	incremental := false
+	return []ent.Field{
+		field.Int64("id").StorageKey("version_id").Positive().Annotations(entsql.Annotation{Incremental: &incremental}),
+		field.String("dimension"),
+		field.Int64("block_x"),
+		field.Int64("block_y"),
+		field.Int64("block_z"),
+		field.String("type_id"),
+		field.Int64("start_tick"),
+		field.Int64("end_tick"),
+		field.String("blob_sha256").MaxLen(64),
+	}
+}
+
+func (BlockEntityVersion) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("dimension", "block_x", "block_y", "block_z", "start_tick").Unique(),
+		index.Fields("dimension", "start_tick", "end_tick"),
+	}
+}
---
+++ databases/scene/ent/schema/player_state.go
@@ -0,0 +1,86 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+	"github.com/proj-airi/recorder-minecraft/pkg/entsqlite"
+)
+
+type PlayerState struct {
+	ent.Schema
+}
+
+func (PlayerState) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{Table: "player_states"},
+		entsqlite.Annotation{ForeignKeys: []entsqlite.ForeignKey{
+			{
+				Symbol:         "fk_player_states_server_tick_frames",
+				Field:          "id",
+				Reference:      "Frame",
+				ReferenceField: "id",
+			},
+		}},
+	}
+}
+
+func (PlayerState) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("entity_version", EntityVersion.Type).Field("entity_version_id").Unique().Required(),
+		edge.To("payload", Blob.Type).Field("payload_sha256").Unique().Required(),
+	}
+}
+
+func (PlayerState) Fields() []ent.Field {
+	incremental := false
+	return []ent.Field{
+		field.Int64("id").StorageKey("server_tick").Annotations(entsql.Annotation{Incremental: &incremental}),
+		field.Int64("entity_version_id"),
+		field.String("entity_instance_id"),
+		field.Int64("entity_id"),
+		field.String("dimension"),
+		field.Float("position_x"),
+		field.Float("position_y"),
+		field.Float("position_z"),
+		field.Float("velocity_x"),
+		field.Float("velocity_y"),
+		field.Float("velocity_z"),
+		field.Float("yaw"),
+		field.Float("pitch"),
+		field.Float("head_yaw"),
+		field.Bool("alive"),
+		field.Bool("on_ground"),
+		field.String("pose").MaxLen(64),
+		field.Bool("sprinting"),
+		field.Bool("sneaking"),
+		field.Bool("swimming"),
+		field.Bool("fall_flying"),
+		field.Bool("using_item"),
+		field.Int("use_item_remaining_ticks"),
+		field.String("game_mode").MaxLen(64),
+		field.Float("health"),
+		field.Float("max_health"),
+		field.Float("absorption"),
+		field.Int("armor"),
+		field.Int("air"),
+		field.Int("max_air"),
+		field.Int("food_level"),
+		field.Float("saturation"),
+		field.Int("experience_level"),
+		field.Float("experience_progress"),
+		field.Int("total_experience"),
+		field.Int("selected_slot"),
+		field.Int64("state_barrier_apply_sequence"),
+		field.String("payload_sha256").MaxLen(64),
+	}
+}
+
+func (PlayerState) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("entity_instance_id"),
+	}
+}
